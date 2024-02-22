075d0ba606d33c46b953a8bfdd6127c3a5e288f9
Patricia Borlongan <pborlongan@goodmorning.com>
2024-02-22 12:36:16 -0700

    Added component assignment
    Made it too complicated. The insructions were unclear so I made the app more complicated than I shoudl've

---
+++ projects/section-eight/cmp-communication-assignment-problem/src/components/ActiveUser.vue
@@ -0,0 +1,24 @@
+<template>
+  <div className="active">
+    <h2>UserName: {{ name }}</h2>
+    <h3>Age: {{ age }}</h3>
+    <p>{{ active ? 'Active' : 'Inactive' }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    name: {
+      type: String,
+    },
+    age: {
+      type: String,
+    },
+    active: {
+      type: Boolean,
+    },
+  },
+  methods: {},
+};
+</script>
---
+++ projects/section-eight/cmp-communication-assignment-problem/src/components/UserData.vue
@@ -0,0 +1,36 @@
+<template>
+  <form @submit.prevent="addUserToList">
+    <div>
+      <label>Name:</label>
+      <input type="text" v-model="name" />
+    </div>
+    <div>
+      <label>Age:</label>
+      <input type="text" v-model="age" />
+    </div>
+    <div>
+      <button>Add</button>
+    </div>
+  </form>
+</template>
+
+<script>
+export default {
+  emits: ['add-user'],
+  data() {
+    return {
+      //passing objects when the array is empty in the parent component is not ideal.
+      id: '',
+      name: '',
+      age: '',
+      active: true,
+    };
+  },
+  methods: {
+    addUserToList() {
+      this.id = new Date().toISOString;
+      this.$emit('add-user', this.id, this.name, this.age, this.active);
+    },
+  },
+};
+</script>
