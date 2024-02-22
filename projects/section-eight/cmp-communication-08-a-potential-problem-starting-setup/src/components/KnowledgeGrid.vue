e0fb325cf138246897fac16254ea47300e5e8fc3
Patricia Borlongan <pborlongan@goodmorning.com>
2024-02-22 13:01:40 -0700

    Added inject and provide examples:

---
+++ projects/section-eight/cmp-communication-08-a-potential-problem-starting-setup/src/components/KnowledgeGrid.vue
@@ -0,0 +1,20 @@
+<template>
+  <ul>
+    <knowledge-element
+      v-for="topic in topics"
+      :key="topic.id"
+      :id="topic.id"
+      :topic-name="topic.title"
+      :description="topic.description"
+    ></knowledge-element>
+  </ul>
+</template>
+
+<script>
+export default {
+  //inject works with provide, used like prop.
+  //only works with parents/ancestor components.
+  //use wisely. only use it when you have to weave through different components to pass data.
+  inject: ['topics'],
+};
+</script>
